641181d0deb4786cb81f0557821932265964aba3
joey500 <joey_gt@msn.com>
2016-04-25 17:04:47 +0200

    nieuw bestand

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        //launch(args);
+        /*int debt=144000;
+        debt=debt/1440;
+        System.out.println("th price is + "debt);
+        */
+
+       // int number =255;
+      //  System.out.println("th price is " + Math.sqrt(number));
+
+    }
+}
